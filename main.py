f00a26a2f18990c12390ebcd892ec4388abe1bac
wuxiangcheng <grow.uo.qiu@qq.com>
2023-06-01 16:20:13 +0800

    update import

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 from setting import InfoUser, Config
 from loguru import logger
 import pyautogui
